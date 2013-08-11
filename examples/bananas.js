0ca5386cdcec41e887c57c8ece62778384f40d01
theorm <theorm@gmail.com>
2013-08-11 12:12:57 +1000

    - refactoring

--- examples/bananas.js
+++ examples/bananas.js
@@ -26,33 +26,44 @@ bananas.find_by_id = function(id) {
   return _.find(this, function(b) {
     return b.id == id;
   });
+};
+
+var BananasApi = function(bananas) {
+  return {
+    list: function(req, res) {
+      var limit = req.query.limit || 10;
+      res.json(bananas.slice(0, limit));
+    },
+    get: function(req, res) {
+      var banana = bananas.find_by_id(req.params.id)
+      if (banana) {
+        res.json(banana);
+      } else {
+        res.send(404);
+      }
+    },
+    post: function(req, res) {
+      var newBanana = req.body;
+
+      if (!newBanana.id) {
+        newBanana.id = new ObjectID();
+      }
+
+      if (bananas.find_by_id(newBanana.id)) {
+        res.send(409, 'This ID already exists.');
+      } else {
+        bananas.push(newBanana);
+        res.send(204);
+      }
+    }
+  }
 }
 
-app.get('/bananas', schema('get_bananas'), function(req, res) {
-  res.json(bananas);
-});
+var api = BananasApi(bananas);
 
-app.post('/bananas', schema('banana'), function(req, res) {
-
-  var newBanana = req.body;
-
-  if (!newBanana.id) {
-    newBanana.id = new ObjectID();
-  }
-
-  if (bananas.find_by_id(newBanana.id)) {
-    res.send(409, 'This ID already exists.')
-  } else {
-    bananas.push(newBanana);
-    res.send(204)
-  }
-
-});
-
-app.get('/bananas/:id', function(req, res) {
-  var banana = bananas.find_by_id(req.params.id)
-  res.json(banana)
-});
+app.get('/bananas', schema('get_bananas'), api.list);
+app.get('/bananas/:id', api.get);
+app.post('/bananas', schema('banana'), api.post);
 
 // ----------------------
 RestSchema.explorer('/spec', app)
